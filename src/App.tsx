d2cb07cded57668a3429762743e196534108dea2
AndriiYelieva <yelieva.a@gmail.com>
2025-11-18 19:41:56 +0100

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from './Pages/Home'
 import { PrivacyPolicy } from './Pages/PrivacyPolicy'
 // import { Donate } from './Pages/Donate';
@@ -10,16 +9,14 @@ import './i18n'
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/donate" element={<Donate />}/> */}
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-use" element={<PrivacyPolicy />} />
-
+        <Route path="/privacy-policy" element={<PrivacyPolicy />}/>
+        <Route path="/terms-of-use" element={<PrivacyPolicy />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
